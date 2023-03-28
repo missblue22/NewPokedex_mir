b3032fdc7a0682613a0a74e3648439fb0b86c7d7
A L <ultimaterror@laposte.net>
2023-03-28 15:53:34 +0200

    Add first props

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,17 +1,6 @@
 import React from "react";
 
-const pokemonList = [
-    {
-        name: "bulbasaur",
-        imgSrc:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-        name: "mew",
-    },
-];
-
-function PokemonCard() {
+function PokemonCard({pokemonList}) {
 
     const pokemon = pokemonList[0];
 
