d7e949db7fb620f0193822dbe54d8c12f1c70b7d
A L <ultimaterror@laposte.net>
2023-03-28 15:47:40 +0200

    Condition in JSX

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,11 +1,30 @@
 import React from "react";
 
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
+
 function PokemonCard() {
+
+    const pokemon = pokemonList[0];
+
     return (
 
         <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="bulbasaur" />
-            <figcaption>Bulbasaur</figcaption>
+            {pokemon.imgSrc === undefined ?
+                <p>???</p>
+                :
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="bulbasaur" />
+            }
+
+            <figcaption>{pokemon.name}</figcaption>
         </figure>
     )
 }
