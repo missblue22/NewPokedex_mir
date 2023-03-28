dc5bf4213ed002f2a700d3117383b35fd70cad71
A L <ultimaterror@laposte.net>
2023-03-28 15:32:00 +0200

    Add first component

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function PokemonCard() {
+    return (
+
+        <figure>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="bulbasaur" />
+            <figcaption>Bulbasaur</figcaption>
+        </figure>
+    )
+}
+
+export default PokemonCard
