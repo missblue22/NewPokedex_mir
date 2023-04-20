59d8333d045a34f87038a711613dba5d7f4125fa
Missblue <mireille.dert@gmail.com>
2023-04-20 14:02:19 +0200

    ajout alert bienvenue

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import PokemonCard from './components/PokemonCard'
-import React from 'react'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import PokemonCard from "./components/PokemonCard";
+import React from "react";
 
 const pokemonList = [
   {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "charmander",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
-    },
-    {
-      name: "squirtle",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
-    },
-    {
-      name: "pikachu",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 function App() {
-  const [pokemonIndex, setPokemonIndex] = useState(0)
- 
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
   const handleNext = () => {
-    setPokemonIndex (pokemonIndex+1)
-  }
+    setPokemonIndex(pokemonIndex + 1);
+  };
   const handlePrevious = () => {
-    setPokemonIndex (pokemonIndex-1)
-  }
-  
+    setPokemonIndex(pokemonIndex - 1);
+  };
+
+  useEffect(() => {
+    alert("Bienvenue Pokemon Fans ;) !");
+  }, 
+  []
+  );
+
   return (
     <div>
-            <PokemonCard  
-                pokemonName={pokemonList[pokemonIndex].name}
-                pokemonIMG={pokemonList[pokemonIndex].imgSrc}
-            />
-            {/*      
+      <PokemonCard
+        pokemonName={pokemonList[pokemonIndex].name}
+        pokemonIMG={pokemonList[pokemonIndex].imgSrc}
+      />
+      {/*      
                 <p>{pokemon.name}</p> 
                   {pokemon.imgSrc} */}
-              <div>
-                  {pokemonIndex>0 ? <button onClick={handlePrevious}>PREVIOUS</button> : null}
-                  {pokemonIndex < pokemonList.length -1 ? <button onClick={handleNext}>NEXT</button> : null}
-              </div>
+      <div>
+        {pokemonIndex > 0 ? (
+          <button onClick={handlePrevious}>PREVIOUS</button>
+        ) : null}
+        {pokemonIndex < pokemonList.length - 1 ? (
+          <button onClick={handleNext}>NEXT</button>
+        ) : null}
+      </div>
     </div>
-  )
+  );
 }
+
+export default App;
